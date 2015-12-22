e6a1d640cd7ba0cfc800ebd110f37fc085b60081
yarik_000 <yarik.red@gmail.com>
2015-12-22 21:30:38 +0200

    22.12.2015

--- js/calc.js
+++ js/calc.js
@@ -15,33 +15,60 @@ var remainCost;
 var remainPrice;
 var quantity;
 
-var numberColors;
-var numberPages;
-var formatPage;
+var numberColors = 4;
+var numberPages = 15000;
+var formatPage = "A4";
 
-quantity =
-formCost = numberColors * formPrice;
-remainCost = (quantity - 1000)*remainPrice / 1000;
-switch (numberColors) {
-    case 1 :
-        baseCost = 25 * exchangeRates;
-        remainPrice = 10 * exchangeRates;
+
+switch (formatPage) {
+    case "A1" :
+        quantity = numberPages
         break
-    case 2 :
-        baseCost = 50 * exchangeRates;
-        remainPrice = 10 * exchangeRates;
+    case "A2" :
+        quantity = numberPages / 2
         break
-    case 3 :
-        baseCost = 70 * exchangeRates;
-        remainPrice = 10 * exchangeRates;
+    case "A3" :
+        quantity = numberPages / 4
         break
-    case 4 :
-        baseCost = 80 * exchangeRates;
-        remainPrice = 10 * exchangeRates;
+    case "A4" :
+        quantity = numberPages / 8
         break
-    default alert("Выберите цвеиность печати.");
+    case "A5" :
+        quantity = numberPages / 16
+        break
+    case "A6" :
+        quantity = numberPages / 32
+        break
+    default:
+        alert("Выберите формат бумаги.")
         break
 }
+
+
+switch (numberColors) {
+    case 1 :
+        baseCost = 25 * exchangeRates
+        remainPrice = 10 * exchangeRates
+        break
+    case 2 :
+        baseCost = 50 * exchangeRates
+        remainPrice = 12 * exchangeRates
+        break
+    case 3 :
+        baseCost = 70 * exchangeRates
+        remainPrice = 15 * exchangeRates
+        break
+    case 4 :
+        baseCost = 80 * exchangeRates
+        remainPrice = 20 * exchangeRates
+        break
+    default:
+        alert("Выберите цвеиность печати.");
+        break
+}
+materialCost = 500;
+formCost = numberColors * formPrice;
+remainCost = (quantity - 1000)*remainPrice / 1000;
 printCost = baseCost + remainCost;
 finalCost = materialCost + formCost + printCost * discount;
 function calculation() {

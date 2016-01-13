42ee78afd184c5b98015980cb4bbba90cb92a9f4
yarik_000 <yarik.red@gmail.com>
2016-01-13 22:51:50 +0200

    13.01.2016

--- save-new-data.php
+++ save-new-data.php
@@ -1,26 +1,8 @@
 <?
-echo $_GET['formPrice'];
-echo $_GET['exchangeRates'];
-echo $_GET['discount'];
-echo $_GET['paperPriceGazetka45'];
-echo $_GET['paperPriceOfset55'];
-echo $_GET['paperPriceOfset60'];
-echo $_GET['paperPriceOfset65'];
-echo $_GET['paperPriceOfset70'];
-echo $_GET['paperPriceOfset80'];
-echo $_GET['paperPriceMelovka65'];
-echo $_GET['paperPriceMelovka70'];
-echo $_GET['paperPriceMelovka80'];
-echo $_GET['paperPriceMelovka90'];
-echo $_GET['paperPriceMelovka115'];
-echo $_GET['paperPriceMelovka130'];
-echo $_GET['paperPriceMelovka150'];
-echo $_GET['paperPriceMelovka170'];
-echo $_GET['paperPriceLegkomelovka52'];
-echo $_GET['paperPriceLegkomelovka60'];
-echo $_GET['paperPriceKarton200'];
-echo $_GET['paperPriceKarton250'];
-echo $_GET['paperPriceKarton300'];
-echo $_GET['paperPriceKarton350'];
-echo $_GET['raflatak'];
+$package = array ('formPrice'=>$_GET['formPrice'],'exchangeRates'=>$_GET['exchangeRates'],'discount'=>$_GET['discount'],'paperPriceGazetka45'=>$_GET['paperPriceGazetka45'],'paperPriceOfset55'=>$_GET['paperPriceOfset55'],'paperPriceOfset60'=>$_GET['paperPriceOfset60'],'paperPriceOfset65'=>$_GET['paperPriceOfset65'],'paperPriceOfset70'=>$_GET['paperPriceOfset70'],'paperPriceOfset80'=>$_GET['paperPriceOfset80'],'paperPriceMelovka65'=>$_GET['paperPriceMelovka65'],'paperPriceMelovka70'=>$_GET['paperPriceMelovka70'],'paperPriceMelovka80'=>$_GET['paperPriceMelovka80'],'paperPriceMelovka90'=>$_GET['paperPriceMelovka90'],'paperPriceMelovka115'=>$_GET['paperPriceMelovka115'],'paperPriceMelovka130'=>$_GET['paperPriceMelovka130'],'paperPriceMelovka150'=>$_GET['paperPriceMelovka150'],'paperPriceMelovka170'=>$_GET['paperPriceMelovka170'],'paperPriceLegkomelovka52'=>$_GET['paperPriceLegkomelovka52'],'paperPriceLegkomelovka60'=>$_GET['paperPriceLegkomelovka60'],'paperPriceKarton200'=>$_GET['paperPriceKarton200'],'paperPriceKarton250'=>$_GET['paperPriceKarton250'],'paperPriceKarton300'=>$_GET['paperPriceKarton300'],'paperPriceKarton350'=>$_GET['paperPriceKarton350'],'raflatak'=>$_GET['raflatak']);
+$jsondata = json_encode($package);
+echo $jsondata;
+$fh=fopen( $_SERVER['DOCUMENT_ROOT']."/js/package.json", "c+");
+fwrite($fh, json_encode($package));
+fclose($fh);
 ?>

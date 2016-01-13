e48db7b9c2c36c4ce15ad6de7c35c79f7746c0c2
yarikRed <yarik.red@gmail.com>
2016-01-13 17:58:34 +0200

    13.01.2016

--- save-new-data.php
+++ save-new-data.php
@@ -1,3 +1,29 @@
 <?
-echo $_GET['formPrice','exchangeRates','discount','paperPriceGazetka45','paperPriceOfset55','paperPriceOfset60','paperPriceOfset65','paperPriceOfset70','paperPriceOfset80','paperPriceMelovka65','paperPriceMelovka70','paperPriceMelovka80','paperPriceMelovka90','paperPriceMelovka115','paperPriceMelovka130','paperPriceMelovka150','paperPriceMelovka170','paperPriceLegkomelovka52','paperPriceLegkomelovka60','paperPriceKarton200','paperPriceKarton250','paperPriceKarton300','paperPriceKarton350','raflatak'];
+echo $_GET['formPrice']
+echo $_GET['exchangeRates']['paperPriceOfset55']['paperPriceOfset60']['paperPriceOfset65']['paperPriceOfset70']['paperPriceOfset80']['paperPriceMelovka65']['paperPriceMelovka70']['paperPriceMelovka80']['paperPriceMelovka90']['paperPriceMelovka115']['paperPriceMelovka130']['paperPriceMelovka150']['paperPriceMelovka170']['paperPriceLegkomelovka52']['paperPriceLegkomelovka60']['paperPriceKarton200']['paperPriceKarton250']['paperPriceKarton300']['paperPriceKarton350']['raflatak'];
+echo $_GET['discount']
+echo $_GET['paperPriceGazetka45']
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+echo $_GET
+
+echo $_GET
 ?>

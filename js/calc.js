3b5ac939361c27f9bfdac8150d5fb269e1f3337a
yarik_000 <yarik.red@gmail.com>
2016-01-18 14:46:45 +0200

    18.01.2016

--- js/calc.js
+++ js/calc.js
@@ -2,69 +2,96 @@
  * Created by yarik_000 on 10.11.2014.
  */
 function calculation() {
-var formPrice = 200;
-var discount = 1;
-var exchangeRates = 22;
-var paperPrice = 1.1;
 
-var finalCost;
-var printCost;
-var materialCost ;
-var formCost;
-var baseCost;
-var remainCost;
-var remainPrice;
-var quantity;
+    var discount = 1;
+    var exchangeRates = 22;
+    var paperPrice = 1.1;
+    var formPrice = 20;
 
-var numberColors = document.querySelector('input[name = "color-scheme"]:checked').value;
-var numberPages = document.getElementById("page-number-first-tab").value;
-var formatPage = document.getElementsByClassName("selected-format-paper");
+    var finalCost;
+    var printCost;
+    var materialCost ;
+    var formCost;
+    var baseCost;
+    var remainCost;
+    var remainPrice;
+    var quantity;
 
-switch (formatPage[0].id) {
-    case "A1" :
-        quantity = numberPages;
-        break;
-    case "A2" :
-        quantity = numberPages / 2;
-        break;
-    case "A3" :
-        quantity = numberPages / 4;
-        break;
-    case "A4" :
-        quantity = numberPages / 8;
-        break;
-    case "A5" :
-        quantity = numberPages / 16;
-        break;
-    case "A6" :
-        quantity = numberPages / 32;
-        break;
-    default:
-        alert("Выберите формат бумаги.");
-        break;
-}
+    var numberColors = document.querySelector('input[name = "color-scheme"]:checked').value;
+    var numberPages = document.getElementById("page-number-first-tab").value;
+    var formatPage = document.getElementsByClassName("selected-format-paper");
 
-switch (numberColors) {
-    case "1" :
-        baseCost = 25 * exchangeRates;
-        remainPrice = 10 * exchangeRates;
-        break;
-    case "2" :
-        baseCost = 50 * exchangeRates;
-        remainPrice = 12 * exchangeRates;
-        break;
-    case "3" :
-        baseCost = 70 * exchangeRates;
-        remainPrice = 15 * exchangeRates;
-        break;
-    case "4" :
-        baseCost = 80 * exchangeRates;
-        remainPrice = 20 * exchangeRates;
-        break;
-    default:
-        alert("Выберите цветность печати.");
-        break;
-}
+
+    function loadJSON(callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', 'js/package.json', true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    function load() {
+
+        loadJSON(function(response) {
+
+            var actual_JSON = JSON.parse(response);
+            console.log(actual_JSON);
+        });
+
+    }
+    load();
+
+    switch (formatPage[0].id) {
+        case "A1" :
+            quantity = numberPages;
+            break;
+        case "A2" :
+            quantity = numberPages / 2;
+            break;
+        case "A3" :
+            quantity = numberPages / 4;
+            break;
+        case "A4" :
+            quantity = numberPages / 8;
+            break;
+        case "A5" :
+            quantity = numberPages / 16;
+            break;
+        case "A6" :
+            quantity = numberPages / 32;
+            break;
+        default:
+            alert("Выберите формат бумаги.");
+            break;
+    }
+
+    switch (numberColors) {
+        case "1" :
+            baseCost = 25 * exchangeRates;
+            remainPrice = 10 * exchangeRates;
+            break;
+        case "2" :
+            baseCost = 50 * exchangeRates;
+            remainPrice = 12 * exchangeRates;
+            break;
+        case "3" :
+            baseCost = 70 * exchangeRates;
+            remainPrice = 15 * exchangeRates;
+            break;
+        case "4" :
+            baseCost = 80 * exchangeRates;
+            remainPrice = 20 * exchangeRates;
+            break;
+        default:
+            alert("Выберите цветность печати.");
+            break;
+    }
+
     materialCost = (1.03*quantity + 270) * paperPrice; console.log("materialCost=", materialCost);
     formCost = numberColors * formPrice; console.log("formCost=", formCost);
     remainCost = (quantity - 1000)*remainPrice / 1000; console.log("remainCost=", remainCost);

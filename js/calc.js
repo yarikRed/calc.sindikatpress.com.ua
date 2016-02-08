f0f0171d453f826be34ebe831d346813e623d76f
yarik_000 <yarik.red@gmail.com>
2016-02-08 21:45:24 +0200

    18.01.2016

--- js/calc.js
+++ js/calc.js
@@ -16,9 +16,10 @@ function calculation() {
     var remainCost;
     var remainPrice;
     var quantity;
+    var numberForms;
 
     var numberColors = document.querySelector('input[name = "color-scheme"]:checked').value;
-    var numberPages = document.getElementById("page-number-first-tab").value;
+    var numberPages = document.getElementById("page-number-first-tab-tab1").value;
     var formatPage = document.getElementsByClassName("selected-format-paper");
 
 
@@ -41,50 +42,88 @@ function calculation() {
 
             var actual_JSON = JSON.parse(response);
             console.log(actual_JSON);
-            alert(actual_JSON);
+            /*alert(actual_JSON);*/
         });
 
     }
     load();
 
     switch (formatPage[0].id) {
-        case "A1" :
+        case "A1-tab1" :
             quantity = numberPages;
             break;
-        case "A2" :
+        case "A2-tab1" :
             quantity = numberPages / 2;
             break;
-        case "A3" :
+        case "A3-tab1" :
             quantity = numberPages / 4;
             break;
-        case "A4" :
+        case "A4-tab1" :
             quantity = numberPages / 8;
             break;
-        case "A5" :
+        case "A5-tab1" :
             quantity = numberPages / 16;
             break;
-        case "A6" :
+        case "A6-tab1" :
+            quantity = numberPages / 32;
+            break;
+        case "B1-tab1" :
+            quantity = numberPages;
+            break;
+        case "B2-tab1" :
+            quantity = numberPages / 2;
+            break;
+        case "B3-tab1" :
+            quantity = numberPages / 4;
+            break;
+        case "B4-tab1" :
+            quantity = numberPages / 8;
+            break;
+        case "B5-tab1" :
+            quantity = numberPages / 16;
+            break;
+        case "B6-tab1" :
             quantity = numberPages / 32;
             break;
         default:
             alert("Выберите формат бумаги.");
             break;
     }
-
+    console.log(numberColors);
     switch (numberColors) {
-        case "1" :
+        case "1+0" :
+        case "1+1" :
+            numberForms = 1;
             baseCost = 25 * exchangeRates;
             remainPrice = 10 * exchangeRates;
             break;
-        case "2" :
+        case "2+0" :
+        case "2+1" :
+        case "2+2" :
+            numberForms = 2;
             baseCost = 50 * exchangeRates;
             remainPrice = 12 * exchangeRates;
             break;
-        case "3" :
+        case "3+0" :
+        case "3+1" :
+        case "3+2" :
+        case "3+3" :
+            numberForms = 3;
             baseCost = 70 * exchangeRates;
             remainPrice = 15 * exchangeRates;
             break;
-        case "4" :
+        case "4+0" :
+        case "4+1" :
+        case "4+2" :
+        case "4+3" :
+        case "4+4" :
+        case "5+0" :
+        case "5+1" :
+        case "5+2" :
+        case "5+3" :
+        case "5+4" :
+        case "5+5" :
+            numberForms = 4;
             baseCost = 80 * exchangeRates;
             remainPrice = 20 * exchangeRates;
             break;
@@ -94,10 +133,10 @@ function calculation() {
     }
 
     materialCost = (1.03*quantity + 270) * paperPrice; console.log("materialCost=", materialCost);
-    formCost = numberColors * formPrice; console.log("formCost=", formCost);
+    formCost = numberForms * formPrice; console.log("formCost=", formCost);
     remainCost = (quantity - 1000)*remainPrice / 1000; console.log("remainCost=", remainCost);
     printCost = baseCost + remainCost; console.log("printCost=", printCost);
-    finalCost = materialCost + formCost + printCost * discount; console.log("finalCost=", finalCost);
+    finalCost = Math.round(materialCost + formCost + printCost * discount); console.log("finalCost=", finalCost);
 
-    document.getElementById("printCost").childNodes[0].nodeValue = finalCost + " грн";
+    document.getElementById("printCost-tab1").childNodes[0].nodeValue = finalCost + " грн";
 }
--- js/ui.js
+++ js/ui.js
@@ -1,48 +1,63 @@
 /**
  * Created by yarik_000 on 10.11.2014.
  */
+
 $(document).ready(function () {
+
     $('.format-paper').click(function (){
         $('div.format-paper').removeClass('selected-format-paper');
        /* $(this).css('background-color', '#08c');*/
         $(this).addClass('selected-format-paper');
-        $('#label-format-selected').addClass('selected-items');
-        $('#label-format-selected').removeClass('unselected-items');
-        $('#label-format-selected').empty();
-        $('#label-format-selected').append($('.selected-format-paper').attr('id'));
+        $('#label-format-selected-tab1').addClass('selected-items');
+        $('#label-format-selected-tab1').removeClass('unselected-items');
+        $('#label-format-selected-tab1').empty();
+        $('#label-format-selected-tab1').append($('.selected-format-paper').data('format'));
     });
 
     $('.color-number').click(function (){
-        $('#label-color-selected').addClass('selected-items');
-        $('#label-color-selected').removeClass('unselected-items');
-        $('#label-color-selected').empty();
-        $('#label-color-selected').append($('input[name = "color-scheme"]:checked').attr('value'));
+        $('#label-color-selected-tab1').addClass('selected-items');
+        $('#label-color-selected-tab1').removeClass('unselected-items');
+        $('#label-color-selected-tab1').empty();
+        $('#label-color-selected-tab1').append($('input[name = "color-scheme"]:checked').attr('value'));
     });
 
     $('.material-kind').click(function (){
-        $('#label-material-selected').addClass('selected-items');
-        $('#label-material-selected').removeClass('unselected-items');
-        $('#label-material-selected').empty();
-        $('#label-material-selected').append($('input[name = "material"]:checked').attr('value'));
+        $('#label-material-selected-tab1').addClass('selected-items');
+        $('#label-material-selected-tab1').removeClass('unselected-items');
+        $('#label-material-selected-tab1').empty();
+        $('#label-material-selected-tab1').append($('input[name = "material"]:checked').attr('value'));
+    });
+
+    $('.varnish').click(function (){
+        $('#label-varnish-selected-tab1').addClass('selected-items');
+        $('#label-varnish-selected-tab1').removeClass('unselected-items');
+        $('#label-varnish-selected-tab1').empty();
+        $('#label-varnish-selected-tab1').append($('input[name = "laminat"]:checked').attr('value'));
+        $('#label-varnish-selected-tab1').append($('input[name = "varnish"]:checked').attr('value'));
+        $('#label-varnish-selected-tab1').append($('input[name = "uf-varnish"]:checked').attr('value'));
     });
 
     $('input[name = "quantity"]').keyup(function (){
-        $('#label-quantity-selected').addClass('selected-items');
-        $('#label-quantity-selected').removeClass('unselected-items');
-        $('#label-quantity-selected').empty();
-        $('#label-quantity-selected').text($('input[name = "quantity"]').val());
+        $('#label-quantity-selected-tab1').addClass('selected-items');
+        $('#label-quantity-selected-tab1').removeClass('unselected-items');
+        $('#label-quantity-selected-tab1').empty();
+        $('#label-quantity-selected-tab1').text($('input[name = "quantity"]').val());
     });
 
     $('input[name = "bends"]').keyup(function (){
-        $('#label-bends-selected').addClass('selected-items');
-        $('#label-bends-selected').removeClass('unselected-items');
-        $('#label-bends-selected').empty();
-        $('#label-bends-selected').text($('input[name = "bends"]').val());
+        $('#label-bends-selected-tab1').addClass('selected-items');
+        $('#label-bends-selected-tab1').removeClass('unselected-items');
+        $('#label-bends-selected-tab1').empty();
+        $('#label-bends-selected-tab1').text($('input[name = "bends"]').val());
     });
 
 
 });
 
+/*var activeTab = $('[data-tab][class = "active"]').attr('id');
+console.log (activeTab);*/
+
 function saveResults() {
-    $('.field-history-btn').append('<div class="alert alert-info" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>    <span class="badge badge-success">1000 грн</span> А3, Цвет 3+1, Глянцевая 100 г/м2, 1000 шт, УФ лакировка 1+1<br><br>    <button class="btn btn-primary">Отправить заказ</button>&nbsp;&nbsp; <button class="btn btn-success">Изменить заказ</button>    </div>');
+    calculation();
+    $('.field-history-btn').append('<div class="alert alert-info" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>    <span class="badge badge-success">'+ $('#printCost-tab1').text() +'</span> '+ $('#label-format-selected-tab1').text() +', Цвет '+ $('#label-color-selected-tab1').text() +', '+ $('#label-material-selected-tab1').text() +', '+ $('#label-quantity-selected-tab1').text() +' шт, Доп. обработка: '+ $('#label-varnish-selected-tab1').text() +', кол-во изгибов: '+ $('#label-bends-selected-tab1').text() +'<br><br>    <button class="btn btn-primary">Отправить заказ</button>&nbsp;&nbsp; <button class="btn btn-success">Изменить заказ</button>    </div>');
 };
